bf6855948984c694a5d3243dbc4a48e3de5a743e
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-22 10:53:04 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,17 @@ class Shoe
   attr_reader :brand
 
   def initialize(brand)
-    @brand = brand
+    self.genre = brand
   end
+
+  BRANDS = []
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  def genre=(brand)
+    @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
+  end
 end
